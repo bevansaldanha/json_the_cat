90f5c41cec9f7b17d153eb9d45b4b358cc7a7c9e
Bevan Saldanha <bevansaldanha15@gmail.com>
2023-09-28 17:34:38 -0400

    created breedFetcher

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require("request");
+const args = process.argv.slice(2);
+
+request(`https://api.thecatawapi.com/v1/breeds/search?q=${args[0]}`, function(error, response, body) {
+
+  if (error !== undefined) {
+    console.log("There was an error");
+    return;
+  }
+  const data = JSON.parse(body);
+  if (data[0] === undefined) {
+    console.log("there was an issue with the breed");
+  }
+  else {
+
+    console.log(data[0]);
+  }
+
+});
